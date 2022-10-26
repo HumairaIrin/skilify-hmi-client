839c4e9a0a30b808d26fdbbcfa2a03f6e60b924a
HumairaIrin <humairamuntaha24@gmail.com>
2022-10-27 02:05:32 +0600

    FAQ component completed

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>this is blog page</h2>
+        <div className='faq-section w-[50%] mx-auto bg-orange-100 p-5 rounded-lg mb-5 md:flex-col'>
+            <h3 className='text-3xl font-bold mb-2' style={{ color: 'rgb(46 87 84)' }}>Some frequently asked questions about our courses!</h3>
+            <div className='questions'>
+                <h2 className='text-2xl'>Will i get the access for lifetime?</h2>
+                <p>Yes.You will get lifetime access.</p>
+            </div>
+            <div className='questions'>
+                <h2 className='text-2xl '>Can I see the bonus videos without finishing the main course videos?</h2>
+                <p>No,we will not give you that access.</p>
+            </div>
+            <div className='questions'>
+                <h2 className='text-2xl '>Is this a freelancer friendly course?</h2>
+                <p>Not really!! But you can build your skills properly with guideline.</p>
+            </div>
+            <div className='questions'>
+                <h2 className='text-2xl '>How many project tasks are there?</h2>
+                <p>There are 10 to 15 projects which will be given you as assignment.</p>
+            </div>
+            <div className='questions'>
+                <h2 className='text-2xl '>How much time will I have to spend regularly to finish regular classes?</h2>
+                <p>It mainly depends on you.But average time is 5 to 7 hours</p>
+            </div>
         </div>
     );
 };
--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import './FAQ.css'
 const FAQ = () => {
     return (
-        <div>
-            <h3>FAQ page</h3>
+        <div className='faq-section w-[50%] mx-auto bg-orange-100 p-5 rounded-lg mb-5 md:flex-col'>
+            <h3 className='text-3xl font-bold mb-2' style={{ color: 'rgb(46 87 84)' }}>Some frequently asked questions about our courses!</h3>
+            <div className='questions'>
+                <h2 className='text-2xl'>Will i get the access for lifetime?</h2>
+                <p>Yes.You will get lifetime access.</p>
+            </div>
+            <div className='questions'>
+                <h2 className='text-2xl '>Can I see the bonus videos without finishing the main course videos?</h2>
+                <p>No,we will not give you that access.</p>
+            </div>
+            <div className='questions'>
+                <h2 className='text-2xl '>Is this a freelancer friendly course?</h2>
+                <p>Not really!! But you can build your skills properly with guideline.</p>
+            </div>
+            <div className='questions'>
+                <h2 className='text-2xl '>How many project tasks are there?</h2>
+                <p>There are 10 to 15 projects which will be given you as assignment.</p>
+            </div>
+            <div className='questions'>
+                <h2 className='text-2xl '>How much time will I have to spend regularly to finish regular classes?</h2>
+                <p>It mainly depends on you.But average time is 5 to 7 hours</p>
+            </div>
         </div>
     );
 };

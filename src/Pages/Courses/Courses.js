374d5550ae946e13fd9d975e7ee8b939abd73203
HumairaIrin <humairamuntaha24@gmail.com>
2022-10-26 22:13:02 +0600

    course section half completed

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Courses.css'
 
 const Courses = () => {
     return (
-        <div className='flex flex-row course-page'>
-            <div>
+        <div className='mx-auto courses-page'>
+            <div className='courses-list'>
                 <h2>courses list</h2>
             </div>
-            <div>
+            <div className='courses-card'>
                 <h3>courses card</h3>
             </div>
         </div>

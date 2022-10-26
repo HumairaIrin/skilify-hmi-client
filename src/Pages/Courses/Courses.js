dba5c14fb2f2e0f1d1c0339eddc1c4c58ab787a8
HumairaIrin <humairamuntaha24@gmail.com>
2022-10-26 21:29:21 +0600

    home section completed

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Courses = () => {
     return (
-        <div>
-            <h3>this is courses page</h3>
+        <div className='flex flex-row course-page'>
+            <div>
+                <h2>courses list</h2>
+            </div>
+            <div>
+                <h3>courses card</h3>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h3 className='text-5xl'>this is home</h3>
+        <div className='home-section'>
+            <div className='flex flex-col w-4/5 mx-auto'>
+                <h1 className='text-4xl font-bold home-header'>Skilify HMI provides you courses relevent to web development</h1>
+                <p className="home-text">If you wonderning why to gain skill on web development you must have noticed that every popular business has its own websites that help the customer to locate the services you provide. Not only for business, but websites are also built for portfolios, blogs, funding, charities, events, and more.</p>
+            </div>
         </div>
     );
 };
